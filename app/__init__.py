76ccb9bc73ae074ff0970d3c1eec096f9f49ee98
nickyhill <nhillengas@vt.edu>
2025-11-07 15:32:51 -0500

    Fixed wrong directory for data

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from storage.storage import Storage
 
 app = Flask(__name__)
-storage = Storage("logs.db", "data/schema.sql")
+storage = Storage("logs.db", "storage/schema.sql")
 
 from app import routes
