3877c08f4bbc83e1ac13c12c22158500252f5010
nickyhill <nhillengas@vt.edu>
2025-11-07 15:48:57 -0500

    Ignore CORS for time being

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from storage.storage import Storage
 
 app = Flask(__name__)
+CORS(app)
 storage = Storage("logs.db", "storage/schema.sql")
 
 from app import routes
