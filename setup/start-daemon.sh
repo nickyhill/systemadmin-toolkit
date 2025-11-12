747997f4e1f5c2ad9540795f8c05f76fe3ac8063
nickyhill <nhillengas@vt.edu>
2025-11-11 20:13:41 -0500

    create the service; and start it

--- setup/start-daemon.sh
+++ setup/start-daemon.sh
@@ -4,24 +4,18 @@ SERVICE_NAME="logpipeline.service" # Replace with the actual service name
 
 echo "Attempting to start $SERVICE_NAME..."
 
-# Check if the service is already running
+# Create SymLink
+sudo ln -s /var/www/sysadmin-toolkit/setup/logpipeline.service /etc/systemd/system/logpipeline.service
+
+sudo systemctl daemon-reload
+# Enable the service
+sudo systemctl enable "$SERVICE_NAME"
+# Start the service
+sudo systemctl start "$SERVICE_NAME"
+
+# Check if the service started successfully
 if systemctl is-active --quiet "$SERVICE_NAME"; then
-    echo "$SERVICE_NAME is already running."
+    echo "$SERVICE_NAME started successfully."
 else
-
-    # Create SymLink
-    sudo ln -s /var/www/sysadmin-toolkit/setup/logpipeline.service /etc/systemd/system/logpipeline.service
-
-    sudo systemctl daemon-reload
-    # Enable the service
-    sudo systemctl enable "$SERVICE_NAME"
-    # Start the service
-    sudo systemctl start "$SERVICE_NAME"
-
-    # Check if the service started successfully
-    if systemctl is-active --quiet "$SERVICE_NAME"; then
-        echo "$SERVICE_NAME started successfully."
-    else
-        echo "Failed to start $SERVICE_NAME. Check logs for details."
-    fi
+    echo "Failed to start $SERVICE_NAME. Check logs for details."
 fi
