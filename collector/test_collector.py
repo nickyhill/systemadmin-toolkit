13e46689157439f14fd53d052a1fe9a02a476af9
nickyhill <nhillengas@vt.edu>
2025-11-25 16:51:58 -0500

    refactor test file

--- collector/test_collector.py
+++ collector/test_collector.py
@@ -6,7 +6,7 @@ import logging
 
 if __name__ == '__main__':
     # initialize logging
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(__name__)
     logger.info("This message will go to stdout and be captured by journalctl.")
     logger.warning("A warning message captured by journalctl.")
